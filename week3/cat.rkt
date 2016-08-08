1bb745d4b88914ac2aee2cfd1f94039056e6cc0d
Terrel Shumway <moan-o-storm-beware-spoke@shumway.us>
2016-08-08 12:32:10 -0600

    complete first iteration of cat world

--- week3/cat.rkt
+++ week3/cat.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname cat-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname cat) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
 ;; cat.rkt
 
@@ -23,8 +23,74 @@ To help you get started, here is a picture of a cat, which we
 have taken from the 2nd edition of the How to Design Programs 
 book on which this course is based.
 ")
+
+
 (require 2htdp/image)
-(define CAT (bitmap "cat1.png"))
+(require 2htdp/universe)
+
+;; A cat that moves from left to right across the scene
+
+;; =================
+;; Constants:
+(define WIDTH 400)
+(define HEIGHT 200)
+(define MTS (empty-scene WIDTH HEIGHT))
+(define CAT-Y (/ HEIGHT 2))
+(define CAT-IMG (bitmap "cat1.png"))
+
+;; =================
+;; Data definitions:
 
 
+;; CatX is Number
+;; interp. the x position of the cat image in screen coordinates
+(define CATX0 0)            ;left edge
+(define CATX1 (/ WIDTH 2))  ;middle
+(define CATX2 WIDTH)        ;right edge
+
+#;
+(define (fn-for-cat-x catx)
+  (... catx))
+
+;; Template rules used:
+;;  - atomic non-distinct: Number
+
+
+;; =================
+;; Functions:
+
+
+;; CatX -> CatX
+;; start the world with (main 0)
+(define (main catx)
+  (big-bang catx                 ; CatX
+            (on-tick advance-cat); CatX -> CatX
+            (to-draw   render)   ; CatX -> Image
+            #;
+            (stop-when ...)      ; CatX -> Boolean
+            #;
+            (on-mouse  ...)      ; CatX Integer Integer MouseEvent -> CatX
+            #;
+            (on-key    ...)))    ; CatX KeyEvent -> CatX
+
+;; CatX -> CatX
+;; produce the next position of the Cat
+(check-expect (advance-cat 0) 1)
+
+;(define (advance-cat catx) 0) ;stub
+
+;; <use template from CatX>
+(define (advance-cat catx)
+  (+ 1 catx))
+
+
+;; CatX -> Image
+;; render the cat at the specified x position on the empty scene.
+(check-expect (render CATX0) (place-image CAT-IMG CATX0 CAT-Y MTS))
+
+; (define (render catx) ...) stub
+
+;; <use template from CatX>
+(define (render catx)
+  (place-image CAT-IMG catx CAT-Y MTS))
 
