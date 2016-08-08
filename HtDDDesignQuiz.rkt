7286a1cafc0785deb994da30e18a6a5bc335fa38
Terrel Shumway <lernisto@users.no-reply.github.com>
2016-08-08 06:36:00 -0600

    quiz starter, edited to remove box cruft

---
+++ HtDDDesignQuiz.rkt
@@ -0,0 +1,64 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDDDesignQuiz) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; HtDD Design Quiz
+
+;; Age is Natural
+;; interp. the age of a person in years
+(define A0 18)
+(define A1 25)
+
+#;
+(define (fn-for-age a)
+  (... a))
+
+;; Template rules used:
+;; - atomic non-distinct: Natural
+
+
+#;
+("
+Problem 1:
+
+Consider the above data definition for the age of a person.
+
+Design a function called teenager? that determines whether a person
+of a particular age is a teenager (i.e., between the ages of 13 and 19).
+")
+
+
+#;
+("Problem 2:
+
+Design a data definition called MonthAge to represent a person's age
+in months.")
+
+
+#;
+("
+Problem 3:
+
+Design a function called months-old that takes a person's age in years 
+and yields that person's age in months.
+")
+
+
+
+#;
+("Problem 4:
+
+Consider a video game where you need to represent the health of your
+character. The only thing that matters about their health is:
+
+  - if they are dead (which is shockingly poor health)
+  - if they are alive then they can have 0 or more extra lives
+
+Design a data definition called Health to represent the health of your
+character.
+
+Design a function called increase-health that allows you to increase the
+lives of a character.  The function should only increase the lives
+of the character if the character is not dead, otherwise the character
+remains dead.
+")
+
